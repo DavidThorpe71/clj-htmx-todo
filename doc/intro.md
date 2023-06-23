05cec93240b77a7bd53ec08c8a719106568663fb
David Thorpe <davidthorpe878@gmail.com>
2023-06-23 19:56:32 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-htmx-todo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_htmx_todo/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-htmx-todo.core-test
+  (:require [clojure.test :refer :all]
+            [clj-htmx-todo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
